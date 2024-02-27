7598762750eb94e8b959185f47367f2899347515
AmanTiwari <er.amantiwari.2002@gmail.com>
2024-02-27 15:27:54 +0530

    Partail Profile Page

--- src/adOns/atoms/SemiCircle.jsx
+++ src/adOns/atoms/SemiCircle.jsx
@@ -1,7 +1,8 @@
 import React from 'react';
 import { View, Image, Text, StyleSheet } from 'react-native';
+import PressButton from './PressButton';
 
-const InvertedPersonInfoSemicircle = ({ imageSource, name, phoneNumber }) => {
+const InvertedPersonInfoSemicircle = ({ imageSource, name, phoneNumber, editMode }) => {
   return (
     <View style={styles.container}>
       <View style={styles.semicircle}>
@@ -9,15 +10,24 @@ const InvertedPersonInfoSemicircle = ({ imageSource, name, phoneNumber }) => {
         <Text style={styles.name}>{name}</Text>
         <Text style={styles.phoneNumber}>{phoneNumber}</Text>
       </View>
+      {editMode === true ? <View style={styles.saveButton}>
+        <PressButton name="Save" />
+      </View> : ''}
     </View>
   );
 };
 
 const styles = StyleSheet.create({
   container: {
+    position: 'relative',
     alignItems: 'center',
     justifyContent: 'flex-end',
     marginBottom: 30, // Adjust as needed
+  },
+  saveButton: {
+    position: 'absolute',
+    top: 225,
+    left: '35%'
   },
   semicircle: {
     width: '100%',
@@ -38,11 +48,11 @@ const styles = StyleSheet.create({
     fontSize: 18,
     fontWeight: 'bold',
     marginBottom: 5,
-    color:'white'
+    color: 'white'
   },
   phoneNumber: {
     fontSize: 16,
-    color:'white'
+    color: 'white'
   },
 });
 
